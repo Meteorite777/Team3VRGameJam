e5bc11bf914c2bf8cfc0b887c3569d62c778859d
wheatgrinder <wheatgrinder@yahoo.com>
2016-10-13 23:26:21 -0700

    added hmd downscale

---
+++ Assets/Team_3_camRigActions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+public class Team_3_camRigActions : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void moveRig(Vector3 newPos)
+    {
+        Debug.Log("moving to:" + newPos);
+       // this.transform.position = newPos;
+       
+    }
+
+}
