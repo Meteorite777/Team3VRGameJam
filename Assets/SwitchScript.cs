a546da14574807c308006ddc9c7938b7ae1571b6
Scyllarus <irdsm1@gmail.com>
2016-10-14 20:57:24 -0400

    Made cubicle scene

---
+++ Assets/SwitchScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchScript : MonoBehaviour {
+
+	// Use this for initialization
+
+public GameObject CameraRig;
+public GameObject CameraRigKyle;
+
+	void Update()
+		{
+			if(Input.GetKeyDown("Application_Menu"))
+			{
+			if(CameraRig.activeSelf == true)
+				{
+					CameraRig.SetActive(false);
+					CameraRigKyle.SetActive(true);
+				}
+			else if(CameraRig.activeSelf == false)
+				{
+				CameraRig.SetActive(true);
+				CameraRigKyle.SetActive(false);
+				}
+			}
+
+		}
+	}
+	
+	// Update is called once per frame
